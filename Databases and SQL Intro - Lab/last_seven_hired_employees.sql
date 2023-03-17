54fc85c9f419615989c497ffd34fc4ff59580ad9
Angel <angelvalangelov@gmail.com>
2023-03-17 22:52:10 +0200

    last_seven_hired_employees

---
+++ Databases and SQL Intro - Lab/last_seven_hired_employees.sql
@@ -0,0 +1,6 @@
+SELECT TOP(7) FirstName, LastName, HireDate
+FROM Employees
+ORDER BY HireDate DESC;
+
+-- Write a SQL query to find last 7 hired employees.
+-- Select their first, last name and their hire date.
