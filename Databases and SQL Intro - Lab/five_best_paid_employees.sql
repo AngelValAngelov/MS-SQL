878ce352aba0552caec724470a4aff8cbd2ad448
Angel <angelvalangelov@gmail.com>
2023-03-18 19:48:05 +0200

    five_best_paid_employees

---
+++ Databases and SQL Intro - Lab/five_best_paid_employees.sql
@@ -0,0 +1,5 @@
+SELECT TOP (5) FirstName, LastName
+FROM Employees
+ORDER BY Salary DESC;
+
+-- Write SQL query to find first and last names about 5 best paid Employees ordered descending by their salary.
