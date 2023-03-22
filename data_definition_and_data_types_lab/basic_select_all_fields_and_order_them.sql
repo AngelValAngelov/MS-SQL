a80633fd490d1669dbf0a62840661dce210ca461
Angel <angelvalangelov@gmail.com>
2023-03-23 00:46:55 +0200

    basic_select_all_fields_and_order_them

---
+++ data_definition_and_data_types_lab/basic_select_all_fields_and_order_them.sql
@@ -0,0 +1,13 @@
+SELECT *
+FROM Towns
+ORDER BY Name;
+
+SELECT *
+FROM Departments
+ORDER BY Name;
+
+SELECT *
+FROM Employees
+ORDER BY Salary DESC;
+
+-- Modify the queries from the previous problem by sorting:
