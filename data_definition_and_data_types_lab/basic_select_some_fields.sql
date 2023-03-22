18510fded83c4f0e50d59104ba7487fb9101d4e8
Angel <angelvalangelov@gmail.com>
2023-03-23 00:47:23 +0200

    basic_select_some_fields

---
+++ data_definition_and_data_types_lab/basic_select_some_fields.sql
@@ -0,0 +1,13 @@
+SELECT Name
+FROM Towns
+ORDER BY Name;
+
+SELECT Name
+FROM Departments
+ORDER BY Name;
+
+SELECT FirstName, LastName, JobTitle, Salary
+FROm Employees
+ORDER BY Salary DESC;
+
+-- Modify the queries from the previous problem to show only some of the columns.
