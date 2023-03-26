9eb5c1a0ddffd2fc8f50bb7f24fcd518e1e75a7d
Angel <angelvalangelov@gmail.com>
2023-03-27 01:26:57 +0300

    biggest_countries_by_population

---
+++ CRUD/biggest_countries_by_population.sql
@@ -0,0 +1,7 @@
+SELECT TOP (30) CountryName, Population
+FROM Countries
+WHERE ContinentCode = 'EU'
+ORDER BY Population DESC , CountryName;
+
+-- Find the 30 biggest countries by population, located in Europe. Display the "CountryName" and "Population".
+-- Order the results by population (from biggest to smallest), then by country alphabetically.
