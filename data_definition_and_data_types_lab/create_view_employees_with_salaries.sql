b7cc7fc223450a1843c41fe1b5ff4a0c12f1f81f
Angel <angelvalangelov@gmail.com>
2023-03-26 18:56:49 +0300

    create_view_employees_with_salaries

---
+++ data_definition_and_data_types_lab/create_view_employees_with_salaries.sql
@@ -0,0 +1,8 @@
+CREATE VIEW V_EmployeesSalaries AS
+SELECT FirstName, LastName, Salary
+FROM Employees;
+
+SELECT *
+FROM V_EmployeesSalaries;
+
+-- Create a SQL query that creates a view "V_EmployeesSalaries" with first name, last name and salary for each employee.
