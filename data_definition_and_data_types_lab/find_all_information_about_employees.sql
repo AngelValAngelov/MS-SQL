99ccb9935c6b8fd6e8ae609238f114d003f51d4b
Angel <angelvalangelov@gmail.com>
2023-03-26 18:57:16 +0300

    find_all_information_about_employees

---
+++ data_definition_and_data_types_lab/find_all_information_about_employees.sql
@@ -0,0 +1,6 @@
+SELECT *
+FROM Employees
+WHERE JobTitle = 'Sales Representative';
+
+-- Create a SQL query that finds all information about
+-- the employees whose job title is "Sales Representative".
