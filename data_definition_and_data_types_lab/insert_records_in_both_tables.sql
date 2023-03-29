4fd493ac42a09df52bb397571f00e0f578b4e1db
Angel <angelvalangelov@gmail.com>
2023-03-29 22:33:19 +0300

    insert_records_in_both_tables

---
+++ data_definition_and_data_types_lab/insert_records_in_both_tables.sql
@@ -0,0 +1,11 @@
+INSERT INTO Towns (id, Name)
+VALUES (1, 'Sofia'),
+       (2, 'Plovdiv'),
+       (3, 'Varna');
+
+INSERT INTO Minions (id, Name, Age, TownId)
+VALUES (1, 'Kevin', 22, 1),
+       (2, 'Bob', 15, 3),
+       (3, 'Steward', NULL, 2);
+
+-- Populate both tables with sample records, given in the table below.
