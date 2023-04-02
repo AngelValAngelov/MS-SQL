19b51b3b0bd5213113f147e2f53282a54e92345b
Angel <angelvalangelov@gmail.com>
2023-04-03 02:31:24 +0300

    FindNamesofAllEmployeesbyLastName

---
+++ Built-inFunctions/FindNamesofAllEmployeesbyLastName.sql
@@ -0,0 +1,6 @@
+SELECT FirstName, LastName
+FROM Employees
+WHERE LastName LIKE '%ei%';
+
+-- Create a SQL query that finds all employees whose last name contains "ei".
+-- As a result, display "FirstName" and "LastName".
