1661d93de00c2aa135e4c34f51e9490e9719734b
Angel <angelvalangelov@gmail.com>
2023-04-02 23:21:24 +0300

    FindAllEmployeesExceptEngineers

---
+++ Built-inFunctions/FindAllEmployeesExceptEngineers.sql
@@ -0,0 +1,6 @@
+SELECT FirstName, LastName
+FROM Employees
+WHERE JobTitle NOT LIKE '%engineer%';
+
+-- Create a SQL query that finds the first and last names of every employee,
+-- whose job title does not contain "engineer".
