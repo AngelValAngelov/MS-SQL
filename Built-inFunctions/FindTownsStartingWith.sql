25e9ee2a653d0660e90d87115800e5c2d26619b1
Angel <angelvalangelov@gmail.com>
2023-04-03 02:37:03 +0300

    FindTownsStartingWith

---
+++ Built-inFunctions/FindTownsStartingWith.sql
@@ -0,0 +1,7 @@
+SELECT TownID, Name
+From Towns
+WHERE Name LIKE '[MKBE]%'
+ORDER BY Name;
+
+-- Create a SQL query that finds all towns with names starting with 'M', 'K', 'B' or 'E'.
+-- Order the result alphabetically by town name.
